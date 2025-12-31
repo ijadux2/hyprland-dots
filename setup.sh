24696fca90fe313b68b63a080a983d82f12602f6
kumar vaibhav <kv26102009@gmail.com>
2025-12-31 22:05:22 +0530

    shells

--- setup.sh
+++ setup.sh
@@ -7,7 +7,8 @@ mkdir -p ~/.config/hypr/
 mkdir -p ~/.config/kitty 
 mkdir -p ~/.config/waybar 
 mkdir -p ~/.config/swaync 
-mkdir -p ~/Pictures/wallpapers/ 
+mkdir -p ~/Pictures/wallpapers/
+mkdir -p ~/.config/nushell/
 
 
 # organising the directories for startup 
@@ -16,6 +17,17 @@ cp -r ./wallpapers/* ~/Pictures/wallpapers/
 cp -r ./swaync/* ~/.config/swaync/ 
 cp -r ./waybar/* ~/.config/waybar/ 
 cp -r ./kitty/* ~/.config/kitty/ 
-cp -r ./zsh/* ~
+
+sleep 1
+
+echo "reqire oh-my-zsh for zsh-shell for setup"
+read -p "need my config y,n >> " ans 
+
+if [[ $ans == y ]]; then
+  command cp -r ./zsh/.zshrc ~ 
+else
+  command echo "moving forward to nushell !"
+fi
+cp -r ./nushell/* ~/.config/nushell/
 
 echo "restart you machine ! "
